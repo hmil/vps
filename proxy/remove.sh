0f6a0592d4e9117b5efe11afb02b8e65bf064eda
Hadrien Milano <hadrien.milano@appdynamics.com>
2017-01-05 19:10:44 -0800

    fix proxy remove script

--- proxy/remove.sh
+++ proxy/remove.sh
@@ -1,15 +1,27 @@
 
 
-setup() {
+remove() {
   rm "/etc/nginx/sites-enabled/$1"
 }
 
-case "$1" in 
-  "travel" )
-    setup travel
+case "$1" in
+  "blog" )
+    remove travel
   ;;
-  "" )
-    setup travel
+  "cloud" )
+    remove travel
+  ;;
+  "files" )
+    remove travel
+  ;;
+  "homepage" )
+    remove travel
+  ;;
+  "transmission" )
+    remove travel
+  ;;
+  "travel" )
+    remove travel
   ;;
   * )
     echo "invalid target: $1"
