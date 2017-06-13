3a510c68cb86cef7baa070900c9ce7f337ff5b58
Ubuntu <ubuntu@server-1.localdomain>
2017-06-13 22:20:14 +0000

    add cloud upgrade script

--- services/cloud/start.sh
+++ services/cloud/start.sh
@@ -1,5 +1,4 @@
 docker-compose up -d
-docker exec -ti cloud_owncloud_1 /bin/bash -c 'apt-get update && apt-get install sudo'
 # Docker adds rules at the beginning of the FORWARD table
 # so we want to reload the firewall to make sure the firewall
 # rule stays on top
