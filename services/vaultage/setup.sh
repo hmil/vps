bb97ca5f88ca2c0b36322eb2ad06d7458b6c615f
Ubuntu <ubuntu@server-1.localdomain>
2018-02-17 23:50:02 +0000

    backups & vaultage

--- services/vaultage/setup.sh
+++ services/vaultage/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
 
-docker run -d --init -p ${PORT_VAULTAGE}:3000 --name vaultage --restart always -v "$DISK_ROOT/vaultage":/home/node/.vaultage hmil/vaultage
-# docker run -d --init -p ${PORT_VAULTAGE}:3000 --name vaultage --restart always -v "/mnt/data/cloud/data/hmil/files/vaultage":/home/node/.vaultage hmil/vaultage
+docker run -d --init -p ${PORT_VAULTAGE}:3000 --name vaultage --restart always -v "$DISK_ROOT/vaultage":/home/node/.vaultage hmil/vaultage:dev
+
+# Not working because owncloud doesn't detect the changes in the filesystem:
+# docker run -d --init -p ${PORT_VAULTAGE}:3000 --name vaultage --restart always -u www-data -v "$DISK_ROOT/cloud/data/files/hmil/files/vaultage":/var/www/.vaultage hmil/vaultage
