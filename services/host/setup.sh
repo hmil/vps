a38236ddd7f7e7673bb969cd40a4edbe24de9f00
Hadrien Milano <hadrien.milano@gmail.com>
2020-04-27 19:45:25 +0200

    upgrade docker-compose

--- services/host/setup.sh
+++ services/host/setup.sh
@@ -14,7 +14,7 @@ service docker start
 
 # Docker-compose setup
 sh <<EOF
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 EOF
 
