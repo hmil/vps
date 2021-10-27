e45369dcc121f67a7e98b16fb86ff74ef8498d8a
Ubuntu <ubuntu@server-1.localdomain>
2021-10-27 20:24:32 +0000

    update node-red image

--- services/node-red/setup.sh
+++ services/node-red/setup.sh
@@ -3,7 +3,5 @@
 CHANNEL="latest"
 
 docker image remove "nodered/node-red-docker:$CHANNEL" || echo "already removed"
-docker run -d --init -p ${PORT_RED}:1880 --name node-red --restart always -v "$DISK_ROOT/node-red":/data "nodered/node-red-docker:$CHANNEL"
+docker run -d --init -p ${PORT_RED}:1880 --name node-red --restart always -v "$DISK_ROOT/node-red":/data "nodered/node-red:$CHANNEL"
 
-# Not working because owncloud doesn't detect the changes in the filesystem:
-# docker run -d --init -p ${PORT_VAULTAGE}:3000 --name vaultage --restart always -u www-data -v "$DISK_ROOT/cloud/data/files/hmil/files/vaultage":/var/www/.vaultage hmil/vaultage
